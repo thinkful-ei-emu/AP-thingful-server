abec3ccb7a24a27bb714e75adab3092051bba7be
Arpan Patel <adp8ke@gmail.com>
2019-07-31 15:33:35 -0400

    did some refactoring before proceeding

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -3,7 +3,7 @@ const app = require('../src/app')
 const helpers = require('./test-helpers')
 const jwt = require('jsonwebtoken')
 
-describe.only('Auth Endpoints', function() {
+describe('Auth Endpoints', function() {
   let db
 
   const { testUsers } = helpers.makeThingsFixtures()
